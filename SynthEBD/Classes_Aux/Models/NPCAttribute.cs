f214557ba6c57f2cf801d53016dd52be001573cc
Piranha91 <piranha9191@gmail.com>
2021-09-20 12:32:12 -0700

    Added models and viewmodels for new Attribute format

--- SynthEBD/Classes_Aux/Models/NPCAttribute.cs
+++ SynthEBD/Classes_Aux/Models/NPCAttribute.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Mutagen.Bethesda.Plugins;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,5 +17,63 @@ namespace SynthEBD
 
         public string Path { get; set; }
         public string Value { get; set; }
+    }
+
+    public class NPCAttributeVoiceType
+    {
+        public NPCAttributeVoiceType()
+        {
+            this.VoiceTypeFormKey = new FormKey();
+        }
+        public FormKey VoiceTypeFormKey { get; set; }
+    }
+
+    public class NPCAttributeClass
+    {
+        public NPCAttributeClass()
+        {
+            this.ClassFormKey = new FormKey();
+        }
+        public FormKey ClassFormKey { get; set; }
+    }
+
+    public class NPCAttributeFactions
+    {
+        public NPCAttributeFactions()
+        {
+            this.FactionFormKeys = new HashSet<FormKey>();
+            this.RankMin = -1;
+            this.RankMax = 100;
+        }
+        public HashSet<FormKey> FactionFormKeys { get; set; }
+        public int RankMin { get; set; }
+        public int RankMax { get; set; }
+    }
+
+    public class NPCAttributeFaceTexture
+    {
+        public NPCAttributeFaceTexture()
+        {
+            this.FaceTextureFormKey = new FormKey();
+        }
+        public FormKey FaceTextureFormKey { get; set; }
+    }
+
+    public class NPCAttributeRace
+    {
+        public NPCAttributeRace()
+        {
+            this.RaceFormKey = new FormKey();
+        }
+        public FormKey RaceFormKey { get; set; }
+    }
+
+    public class NPCAttributeNPC
+    {
+        public NPCAttributeNPC()
+        {
+            this.NPCFormKey = new FormKey();
+        }
+        public FormKey NPCFormKey { get; set; }
     }
 }
--- SynthEBD/Classes_Aux/ViewModels/VM_NPCAttribute.cs
+++ SynthEBD/Classes_Aux/ViewModels/VM_NPCAttribute.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Mutagen.Bethesda.Plugins;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
@@ -13,14 +14,17 @@ namespace SynthEBD
         {
             this.Path = "";
             this.Value = "";
+            this.GroupedAttributes = new ObservableCollection<object>();
 
             DeleteCommand = new RelayCommand(canExecute: _ => true, execute: _ => parentCollection.Remove(this));
         }
 
         public string Path { get; set; }
         public string Value { get; set; }
+        public ObservableCollection<object> GroupedAttributes { get; set; } // everything within this collection is evaluated as AND (all must be true)
 
         public RelayCommand DeleteCommand { get; }
+
 
         public static VM_NPCAttribute GetViewModelFromModel(NPCAttribute model, ObservableCollection<VM_NPCAttribute> parentCollection)
         {
@@ -39,6 +43,89 @@ namespace SynthEBD
                 oc.Add(GetViewModelFromModel(m, oc));
             }
             return oc;
+        }
+
+
+        public class VM_NPCAttributeVoiceType
+        {
+            public VM_NPCAttributeVoiceType(VM_NPCAttribute parentVM)
+            {
+                this.VoiceTypeFormKey = new FormKey();
+                this.ParentVM = parentVM;
+                DeleteCommand = new RelayCommand(canExecute: _ => true, execute: _ => parentVM.GroupedAttributes.Remove(this));
+            }
+            public FormKey VoiceTypeFormKey { get; set; }
+            public VM_NPCAttribute ParentVM { get; set; }
+            public RelayCommand DeleteCommand { get; }
+        }
+
+        public class VM_NPCAttributeClass
+        {
+            public VM_NPCAttributeClass(VM_NPCAttribute parentVM)
+            {
+                this.ClassFormKey = new FormKey();
+                this.ParentVM = parentVM;
+                DeleteCommand = new RelayCommand(canExecute: _ => true, execute: _ => parentVM.GroupedAttributes.Remove(this));
+            }
+            public FormKey ClassFormKey { get; set; }
+            public VM_NPCAttribute ParentVM { get; set; }
+            public RelayCommand DeleteCommand { get; }
+        }
+
+        public class VM_NPCAttributeFactions
+        {
+            public VM_NPCAttributeFactions(VM_NPCAttribute parentVM)
+            {
+                this.FactionFormKeys = new HashSet<FormKey>();
+                this.RankMin = -1;
+                this.RankMax = 100;
+                this.ParentVM = parentVM;
+                DeleteCommand = new RelayCommand(canExecute: _ => true, execute: _ => parentVM.GroupedAttributes.Remove(this));
+            }
+            public HashSet<FormKey> FactionFormKeys { get; set; }
+            public int RankMin { get; set; }
+            public int RankMax { get; set; }
+            public VM_NPCAttribute ParentVM { get; set; }
+            public RelayCommand DeleteCommand { get; }
+        }
+
+        public class VM_NPCAttributeFaceTexture
+        {
+            public VM_NPCAttributeFaceTexture(VM_NPCAttribute parentVM)
+            {
+                this.FaceTextureFormKey = new FormKey();
+                this.ParentVM = parentVM;
+                DeleteCommand = new RelayCommand(canExecute: _ => true, execute: _ => parentVM.GroupedAttributes.Remove(this));
+            }
+            public FormKey FaceTextureFormKey { get; set; }
+            public VM_NPCAttribute ParentVM { get; set; }
+            public RelayCommand DeleteCommand { get; }
+        }
+
+        public class VM_NPCAttributeRace
+        {
+            public VM_NPCAttributeRace(VM_NPCAttribute parentVM)
+            {
+                this.RaceFormKey = new FormKey();
+                this.ParentVM = parentVM;
+                DeleteCommand = new RelayCommand(canExecute: _ => true, execute: _ => parentVM.GroupedAttributes.Remove(this));
+            }
+            public FormKey RaceFormKey { get; set; }
+            public VM_NPCAttribute ParentVM { get; set; }
+            public RelayCommand DeleteCommand { get; }
+        }
+
+        public class VM_NPCAttributeNPC
+        {
+            public VM_NPCAttributeNPC(VM_NPCAttribute parentVM)
+            {
+                this.NPCFormKey = new FormKey();
+                this.ParentVM = parentVM;
+                DeleteCommand = new RelayCommand(canExecute: _ => true, execute: _ => parentVM.GroupedAttributes.Remove(this));
+            }
+            public FormKey NPCFormKey { get; set; }
+            public VM_NPCAttribute ParentVM { get; set; }
+            public RelayCommand DeleteCommand { get; }
         }
     }
     
