d9b131b2f116d51dfc3da58d051dbdf829942689
Piranha91 <piranha9191@gmail.com>
2022-08-01 01:00:32 -0700

    Fixed headpart display gender filtering

--- SynthEBD/Classes_Aux/ViewModels/VM_HeadPartList.cs
+++ SynthEBD/Classes_Aux/ViewModels/VM_HeadPartList.cs
@@ -12,37 +12,22 @@ using System.Threading.Tasks;
 
 namespace SynthEBD
 {
-    public class VM_HeadPartList
+    public class VM_HeadPartList : VM
     {
         public VM_HeadPartList(HeadPart.TypeEnum type, ObservableCollection<VM_RaceGrouping> raceGroupingVMs, VM_Settings_Headparts parentConfig, VM_SettingsOBody oBody)
         {
             HeadPartType = type;
             DisplayedRuleSet = new(raceGroupingVMs, parentConfig, oBody);
 
-            //this.WhenAnyValue(x => x.GenderToggle).Subscribe(x => UpdateList());
-
-            /*this.WhenAnyValue(
-               x => x.bDisplayMale,
-               x => x.bDisplayFemale,
-               // Just pass along the signal, don't care about the triggering values
-               (_, _) => Unit.Default)
-           .Throttle(TimeSpan.FromMilliseconds(100), RxApp.MainThreadScheduler)
-           .Subscribe(_ => UpdateList());*/
-            TestProperty += HeadPartType + "s!";
-
-            this.WhenAnyValue(x => x.bDisplayFemale).Subscribe(x => UpdateList());
-            this.WhenAnyValue(x => x.bDisplayMale).Subscribe(x => UpdateList());
+            this.WhenAnyValue(x => x.GenderToggle).Subscribe(x => UpdateList());
             HeadPartList.ToObservableChangeSet().Subscribe(_ => UpdateList());
         }
 
         public ObservableCollection<VM_HeadPart> HeadPartList { get; set; } = new();
         public ObservableCollection<VM_HeadPart> DisplayedList { get; set; } = new();
-        public VM_HeadPart DisplayedHeadPart { get; set; }
+        public VM_HeadPart DisplayedHeadPart { get; set; } // for reference only - currently not used for anything but may be useful at some point to be able to tell which headpart category this instance of VM_HeadPartList is for.
         public VM_HeadPartCategoryRules DisplayedRuleSet { get; set; }
-        //public DisplayGender GenderToggle { get; set; } = DisplayGender.Both; 
-        public bool bDisplayMale { get; set; } = true;
-        public bool bDisplayFemale { get; set; } = true;
-        public string TestProperty { get; set; } = "I command you to bind: ";
+        public DisplayGender GenderToggle { get; set; } = DisplayGender.Both; 
         private HeadPart.TypeEnum HeadPartType { get; set; }
 
         public void UpdateList()
@@ -50,8 +35,7 @@ namespace SynthEBD
             DisplayedList.Clear();
             foreach (var headPart in HeadPartList)
             {
-                if (headPart.bAllowMale && bDisplayMale || headPart.bAllowFemale && bDisplayFemale)
-                //if (GenderToggle == DisplayGender.Both || (GenderToggle == DisplayGender.Male && headPart.bAllowMale) || (GenderToggle == DisplayGender.Female && headPart.bAllowFemale))
+                if (GenderToggle == DisplayGender.Both || (GenderToggle == DisplayGender.Male && headPart.bAllowMale) || (GenderToggle == DisplayGender.Female && headPart.bAllowFemale))
                 {
                     DisplayedList.Add(headPart);
                 }
