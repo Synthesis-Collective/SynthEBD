38814e6f7a4d76308372a867a3bb3986dff0b833
Piranha91 <piranha9191@gmail.com>
2023-01-06 23:12:38 -0800

    Ensured vortex virtualization of jcontainers domain

--- SynthEBD/Patcher/PatcherIO/JContainersDomain.cs
+++ SynthEBD/Patcher/PatcherIO/JContainersDomain.cs
@@ -25,6 +25,8 @@ public class JContainersDomain
         string domainPath = Path.Combine(_paths.OutputDataFolder, "SKSE", "Plugins", "JCData", "Domains", "PSM_SynthEBD");
         PatcherIO.CreateDirectoryIfNeeded(domainPath, PatcherIO.PathType.Directory);
 
+        Task.Run(() =>  PatcherIO.WriteTextFile(Path.Combine(domainPath, "SynthEBD.txt"), "This file exists only to make sure that Vortex deploys the containing folder, without which some of SynthEBD's functionality doesn't work. But since you're here, please consider endorsing ;)", _logger));
+
         string domainScriptPath = Path.Combine(_stateProvider.InternalDataPath, "JContainers Domain", "PSM_SynthEBD.pex");
         string domainScriptDestPath = Path.Combine(_paths.OutputDataFolder, "Scripts", "PSM_SynthEBD.pex");
         _patcherIO.TryCopyResourceFile(domainScriptPath, domainScriptDestPath, _logger);
