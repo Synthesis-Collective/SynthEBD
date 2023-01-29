c3c8309fc1dcf70540f94ab1561a66ddea34d8cc
Piranha91 <piranha9191@gmail.com>
2023-01-28 20:52:42 -0800

    Function rename

--- SynthEBD/ViewModelLoader.cs
+++ SynthEBD/ViewModelLoader.cs
@@ -177,7 +177,7 @@ namespace SynthEBD
 
         public void LoadFinalSettingsViewModels() // view models that should be loaded after plugin VMs because they depend on the loaded plugins
         {
-            _texMeshSettingsVM.AssetOrderingMenu.CopyInFromViewModel(_patcherState.TexMeshSettings.AssetOrder);
+            _texMeshSettingsVM.AssetOrderingMenu.CopyInFromModel(_patcherState.TexMeshSettings.AssetOrder);
             _headPartSettingsVM.CopyInFromModel(_patcherState.HeadPartSettings, _generalSettingsVM.RaceGroupingEditor.RaceGroupings);
 
             // load specific assignments (must load after plugin view models)
