47c564c103715eee3b7ce87e8a9c21032741ea2a
Piranha91 <piranha9191@gmail.com>
2023-02-06 01:01:27 -0800

    Version update

--- SynthEBD/PatcherState.cs
+++ SynthEBD/PatcherState.cs
@@ -6,7 +6,7 @@ namespace SynthEBD;
 public class PatcherState
 {
     // Version
-    public static string Version = "0.9.3.2";
+    public static string Version = "0.9.4.1";
     // Settings
     public Settings_General GeneralSettings { get; set; }
     public Settings_TexMesh TexMeshSettings { get; set; }
