25223bbb2d0a693d816d13404de66df421e678cd
Piranha91 <Piranah9191@gmail.com>
2023-04-01 23:06:26 -0700

    Version Display Bump

--- SynthEBD/PatcherState.cs
+++ SynthEBD/PatcherState.cs
@@ -6,7 +6,7 @@ namespace SynthEBD;
 public class PatcherState
 {
     // Version
-    public static string Version = "0.9.9.3";
+    public static string Version = "1.0.0.0";
 
     // Settings
     public Settings_General GeneralSettings { get; set; }
