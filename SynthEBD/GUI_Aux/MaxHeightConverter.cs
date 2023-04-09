54bdfbd7cc6327923d45f8ccd123f6467438d28d
Piranha91 <piranha9191@gmail.com>
2023-04-09 10:28:13 -0700

    Layout change for specific NPC assignment UI

--- SynthEBD/GUI_Aux/MaxHeightConverter.cs
+++ SynthEBD/GUI_Aux/MaxHeightConverter.cs
@@ -12,7 +12,21 @@ namespace SynthEBD
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            double pctHeight = (double)parameter;
+            double pctHeight = -1;
+
+            string aParam = parameter as string;
+            if (aParam != null && double.TryParse(aParam, out double d))
+            {
+                pctHeight = d;
+            }
+            else if (aParam != null)
+            {
+                throw new Exception("MaxHeightConverter expects an argument of type double");
+            }
+            else if (aParam == null)
+            {
+                pctHeight = (double)parameter;
+            }
 
             if ((pctHeight <= 0.0) || (pctHeight > 100.0))
                 throw new Exception("MaxHeightConverter expects parameter in the range (0,100]");
