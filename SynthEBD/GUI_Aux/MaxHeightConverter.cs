1f41a661fe03066e703990c54e678da440550058
Piranha91 <piranha9191@gmail.com>
2023-04-18 22:25:53 -0700

    MaxHeightConverter fails silently

--- SynthEBD/GUI_Aux/MaxHeightConverter.cs
+++ SynthEBD/GUI_Aux/MaxHeightConverter.cs
@@ -12,26 +12,26 @@ namespace SynthEBD
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            double pctHeight = -1;
+            double fracHeight = 1;
 
             string aParam = parameter as string;
             if (aParam != null && double.TryParse(aParam, out double d))
             {
-                pctHeight = d;
+                fracHeight = d;
+            }
+            else if (aParam != null && int.TryParse(aParam, out int i))
+            {
+                fracHeight = System.Convert.ToDouble(i);
             }
             else if (aParam != null)
             {
-                throw new Exception("MaxHeightConverter expects an argument of type double");
-            }
-            else if (aParam == null)
-            {
-                pctHeight = (double)parameter;
+                //throw new Exception("MaxHeightConverter expects an argument of type double"); Fail silently
             }
 
-            if ((pctHeight <= 0.0) || (pctHeight > 100.0))
-                throw new Exception("MaxHeightConverter expects parameter in the range (0,100]");
+            //if ((fracHeight <= 0.0) || (fracHeight > 1.0))
+            //    throw new Exception("MaxHeightConverter expects parameter in the range (0,1)");
 
-            return ((double)value * pctHeight);
+            return ((double)value * fracHeight);
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
