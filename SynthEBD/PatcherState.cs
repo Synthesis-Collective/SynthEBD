b0def41bf691f4abd4d398435abb92ee26606ba1
Piranha91 <Piranah9191@gmail.com>
2023-06-22 18:46:45 -0700

    Version display bump
    Forgot this in 1.0.0.6

--- SynthEBD/PatcherState.cs
+++ SynthEBD/PatcherState.cs
@@ -6,7 +6,7 @@ namespace SynthEBD;
 public class PatcherState
 {
     // Version
-    public static string Version = "1.0.0.5";
+    public static string Version = "1.0.0.7";
 
     // Settings
     public Settings_General GeneralSettings { get; set; }
