3acf66e840666117338b2de6eae9763c785af210
Piranha91 <piranha9191@gmail.com>
2023-09-05 23:33:33 -0700

    Config drafter colorized multiplet display

--- SynthEBD/GUI_Aux/ViewModels/VM_ConfigDrafter.cs
+++ SynthEBD/GUI_Aux/ViewModels/VM_ConfigDrafter.cs
@@ -6,11 +6,15 @@ using ReactiveUI;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Windows;
 using static Mutagen.Bethesda.Plugins.Binary.Processing.BinaryFileProcessor;
 using static SynthEBD.VM_CollectionMemberStringCheckboxList;
 
@@ -500,3 +504,25 @@ public enum DrafterBodyType
     CBBE_3BA,
     BHUNP
 }
+
+public class BoolToSolidColorBrushConverter : IValueConverter
+{
+    public SolidColorBrush TrueColor { get; set; }
+    public SolidColorBrush FalseColor { get; set; }
+
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is bool boolValue)
+        {
+            return boolValue ? TrueColor : FalseColor;
+        }
+
+        return DependencyProperty.UnsetValue; // Return an unset value if the input is not a bool
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException(); // You don't need this for one-way conversion
+    }
+}
+
