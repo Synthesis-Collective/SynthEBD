64555e8dcd0884ce91551478d5c2a20b2c11b048
Piranha91 <piranha9191@gmail.com>
2023-11-20 19:42:42 -0800

    Added masterlist printout for too many masters exception

--- SynthEBD/General_Aux/ExceptionLogger.cs
+++ SynthEBD/General_Aux/ExceptionLogger.cs
@@ -9,6 +9,14 @@ public class ExceptionLogger
     private static string GetExceptionStack(Exception e, string error, int layer)
     {
         error += Environment.NewLine + "======= Layer " + layer.ToString() + ": " + Environment.NewLine + e.Message + Environment.NewLine + e.StackTrace + Environment.NewLine + Environment.NewLine;
+
+        if (e as Mutagen.Bethesda.Plugins.Exceptions.TooManyMastersException != null)
+        {
+            var tooManyMastersEx = e as Mutagen.Bethesda.Plugins.Exceptions.TooManyMastersException;
+
+            error += "Current Masters:" + Environment.NewLine + String.Join(Environment.NewLine, tooManyMastersEx.Masters.Select(x => x.FileName).ToArray()) + Environment.NewLine + Environment.NewLine;
+        }
+
         if (e.InnerException != null)
         {
             return GetExceptionStack(e.InnerException, error, layer + 1);
