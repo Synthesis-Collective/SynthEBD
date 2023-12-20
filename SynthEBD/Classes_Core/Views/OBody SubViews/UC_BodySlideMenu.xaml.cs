dcabf0468406c187b861d30ae23fd604e71b1a89
Piranha91 <piranha9191@gmail.com>
2023-12-20 14:16:58 -0800

    Improved BodySlides menu layout

--- SynthEBD/Classes_Core/Views/OBody SubViews/UC_BodySlideMenu.xaml.cs
+++ SynthEBD/Classes_Core/Views/OBody SubViews/UC_BodySlideMenu.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Controls;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
 
 namespace SynthEBD;
 
@@ -10,5 +13,31 @@ public partial class UC_BodySlideMenu : UserControl
     public UC_BodySlideMenu()
     {
         InitializeComponent();
+    }
+    private void TextBlock_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        if (sender is TextBlock textBlock)
+        {
+            // Find the parent ListBox
+            ListBox listBox = FindVisualParent<ListBox>(textBlock);
+
+            // Handle your logic here
+            // You can access the DataContext of the TextBlock, which is your data item
+            if (listBox != null)
+            {
+                listBox.SelectedItem = textBlock.DataContext;
+            }
+        }
+    }
+
+    private static T FindVisualParent<T>(DependencyObject child) where T : DependencyObject
+    {
+        DependencyObject parentObject = VisualTreeHelper.GetParent(child);
+
+        if (parentObject == null)
+            return null;
+
+        T parent = parentObject as T;
+        return parent ?? FindVisualParent<T>(parentObject);
     }
 }
