b8849db1da6346efad125c49e2094b9fbe15ca80
Piranha91 <piranha9191@gmail.com>
2023-12-23 00:44:25 -0800

    Added VR ESL support

--- SynthEBD/Patcher/PatcherIO/EBDScripts.cs
+++ SynthEBD/Patcher/PatcherIO/EBDScripts.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.Intrinsics.X86;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -25,7 +26,7 @@ namespace SynthEBD
         public void ApplyFixedScripts()
         {
             string sourcePath = String.Empty;
-            if ((_environmentProvider.SkyrimVersion == Mutagen.Bethesda.Skyrim.SkyrimRelease.SkyrimSE && !_patcherState.TexMeshSettings.bFixedScriptsOldSKSEversion) || _environmentProvider.SkyrimVersion == Mutagen.Bethesda.Skyrim.SkyrimRelease.EnderalSE)
+            if ((_environmentProvider.SkyrimVersion == Mutagen.Bethesda.Skyrim.SkyrimRelease.SkyrimSE && !_patcherState.TexMeshSettings.bFixedScriptsOldSKSEversion) || _environmentProvider.SkyrimVersion == Mutagen.Bethesda.Skyrim.SkyrimRelease.SkyrimVR && HasESLVR() || _environmentProvider.SkyrimVersion == Mutagen.Bethesda.Skyrim.SkyrimRelease.EnderalSE)
             {
                 _logger.LogMessage("Applying fixed EBD script (for SSE 1.5.97 or newer)");
                 sourcePath = Path.Combine(_environmentProvider.InternalDataPath, "EBD Code", "SSE", "EBDGlobalFuncs.pex");
@@ -37,6 +38,21 @@ namespace SynthEBD
             }
             string destPath = Path.Combine(_paths.OutputDataFolder, "Scripts", "EBDGlobalFuncs.pex");
             _patcherIO.TryCopyResourceFile(sourcePath, destPath, _logger);
+        }
+
+        private bool HasESLVR()
+        {
+            string expectedDLLpath = Path.Combine(_environmentProvider.DataFolderPath, "SKSE", "Plugins", "skyrimvresl.dll");
+            bool exists = File.Exists(expectedDLLpath);
+            if (exists)
+            {
+                _logger.LogMessage("Detected Skyrim VR ESL Support"); // ESL support comes with ported SKSE scripts that enabled the new EBDGlobalFuncs.pex to work. It does not work with the original SKSEVR scripts, so the old pre-1.5.97 version of EBDGlobalFuncs.pex must be used in that case.
+            }
+            else
+            {
+                _logger.LogMessage("Did not detect Skyrim VR ESL Support. Using EBD Script for SSE < 1.5.97");
+            }
+            return exists;
         }
     }
 }
