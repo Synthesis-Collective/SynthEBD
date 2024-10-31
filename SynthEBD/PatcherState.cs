8edc056059ce2f042988678c034eb8fb13e547d9
A Piranha <piranha9191@gmail.com>
2024-10-31 00:04:55 -0700

    Patcher version bump

--- SynthEBD/PatcherState.cs
+++ SynthEBD/PatcherState.cs
@@ -7,7 +7,7 @@ namespace SynthEBD;
 public class PatcherState
 {
     // Version
-    public static string Version = "1.0.4.1";
+    public static string Version = "1.0.4.2";
 
     // Settings
     public Settings_General GeneralSettings { get; set; }
