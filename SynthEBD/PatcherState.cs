2824a08466918bf7e4e421f33cfa9f3c7bf5b771
A Piranha <piranha9191@gmail.com>
2025-11-09 07:37:49 -0800

    Version and Eval Bump

--- SynthEBD/PatcherState.cs
+++ SynthEBD/PatcherState.cs
@@ -7,7 +7,7 @@ namespace SynthEBD;
 public class PatcherState
 {
     // Version
-    public static string Version = "1.0.6.0";
+    public static string Version = "1.0.6.1";
     // Settings
     public Settings_General GeneralSettings { get; set; }
     public Settings_TexMesh TexMeshSettings { get; set; }
